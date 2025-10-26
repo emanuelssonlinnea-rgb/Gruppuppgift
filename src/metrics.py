5eb753308cb2de5a8b2882b7164479e4a90cf76c
Isabel Holm <isabel.c.holm@gmail.com>
2025-10-26 10:45:22 +0100

    revising notebook and moving code to py

--- src/metrics.py
+++ src/metrics.py
@@ -6,3 +6,32 @@
 # Intäkt per kategori
 # Intäkt per stad
 # Top-3 kategorier efter intäkt
+
+import pandas as pd
+
+# Read the data
+df = pd.read_csv("../data/clean_data.csv")
+
+# Calculating total AOV for the period
+total_aov = df["revenue"].sum() / df["order_id"].count()
+total_aov_rounded = round(total_aov)
+
+# Checking order date is actually a datetime type
+df["date"] = pd.to_datetime(df["date"])
+
+# Extracting month to get AOV per month
+df["month"] = df["date"].dt.to_period("M")
+
+# Group by month and calculate AOV, round to whole number
+monthly_aov = (df.groupby("month")
+               .apply(lambda x: x["revenue"].sum() / x["order_id"].count())
+                      .reset_index(name="AOV")
+                      )
+
+monthly_aov_rounded = round(monthly_aov)
+
+# Sorting by month
+monthly_aov_rounded = monthly_aov_rounded.sort_values("month")
+
+print("Total AOV in the period:", total_aov_rounded)
+print(monthly_aov_rounded)
