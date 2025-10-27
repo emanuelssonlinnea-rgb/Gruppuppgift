8a1771cd012d4134bc0f8353852108dfec2eaa2f
Irene <irenegrisenti@proton.me>
2025-10-27 09:14:57 +0100

    Intäkt per stad

--- src/metrics.py
+++ src/metrics.py
@@ -10,7 +10,7 @@
 import pandas as pd
 
 #-----------BERÄKNINGAR AOV--------------
-def calculate_aov(path="data/clean_data.csv"):
+def calculate_aov(path="Gruppuppgift/data/clean_data.csv"):
     df = pd.read_csv(path)
 
 # Konverting order date to a datetime type
@@ -29,3 +29,10 @@ def calculate_aov(path="data/clean_data.csv"):
     monthly_aov["AOV"] = monthly_aov["AOV"].round(0).astype(int)
     monthly_aov = monthly_aov.sort_values("month")
     return monthly_aov, round(total_aov)
+
+
+# Intäkt per stad
+df = pd.read_csv("Gruppuppgift/data/clean_data.csv")
+
+revenue_per_city = df.groupby("city")["revenue"].sum().sort_values(ascending=False)
+df_revenue_city = revenue_per_city.reset_index()
--- src/viz.py
+++ src/viz.py
@@ -32,3 +32,19 @@ plt.tight_layout()
 
 plt.show()
 #________________________________________
+
+# --- Intäkt per stad ---
+from metrics import df_revenue_city 
+
+# Scaling to thousands for readability purposes
+df_revenue_city["revenue_thousands"] = df_revenue_city["revenue"] / 1000
+
+# Bar chart to show revenue per city
+fig, ax = plt.subplots()
+ax.bar(df_revenue_city["city"], df_revenue_city["revenue_thousands"], color="LightGreen")
+ax.set_title("Total revenue per city")
+ax.set_xlabel ("City")
+ax.set_ylabel("Revenue (thousands)")
+ax.grid(True, axis="y")
+plt.tight_layout()
+plt.show()
