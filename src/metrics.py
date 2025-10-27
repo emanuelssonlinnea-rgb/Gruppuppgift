d5925e5a4909cbe09feec288c20ad9bb8f8724c0
Jakob Ohlsén S. <jowlen@hotmail.se>
2025-10-27 09:25:09 +0100

    Förfintar metrics-dokumentet

--- src/metrics.py
+++ src/metrics.py
@@ -1,8 +1,9 @@
 # Intäkter per kategori
 
-# Kräver att man först öppnar en csv och därefter kör "variabel" = csvreader, och matar sedan in variabeln i denna funktion
+# Kräver att man först öppnar en csv och därefter kör "variabel" = csvreader, och matar sedan in variabeln i revenue_per_category(reader)
 import csv
-
+import numpy as np
+import matplotlib as plt
 
 def revenue_per_category(reader): 
     dictionary_of_category_and_revenue = {}
@@ -21,21 +22,8 @@ def revenue_per_category(reader):
         list_of_category_and_revenue.append((category, revenue))
     
     return sorted(list_of_category_and_revenue, key=lambda item: item[1], reverse=True)
-    
-
-# Nedan testar om funktionen fungerar
-#with open("data/ecommerce_sales.csv", "r", encoding="utf-8") as files:
-#    reader = csv.reader(files)
-#    dictionary_of_test = revenue_per_category(reader)
-#    print(dictionary_of_test)
 
 def top_3_categories(reader):
     top_list_of_categories = revenue_per_category(reader)
     return top_list_of_categories[0:3]
-
-with open("data/ecommerce_sales.csv", "r", encoding="utf-8") as files:
-    reader = csv.reader(files)
-    dictionary_of_test = top_3_categories(reader)
-    print(dictionary_of_test)
-
 
